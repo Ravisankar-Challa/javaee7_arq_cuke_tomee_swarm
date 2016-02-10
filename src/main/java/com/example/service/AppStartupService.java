3e1154d588749aabe650cc000f3553e148af696f
Ravisankar Challa <ravisankar.c@tcs.com>
2016-02-10 16:48:22 +1100

    Added 2 members

--- src/main/java/com/example/service/AppStartupService.java
+++ src/main/java/com/example/service/AppStartupService.java
@@ -5,6 +5,10 @@ import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
+import javax.inject.Inject;
+
+import com.example.entity.Member;
+import com.example.repository.MemberRepository;
 
 @Startup
 @Singleton(name="SomeRandonName")
@@ -12,9 +16,22 @@ public class AppStartupService {
 	
 	private static final Logger LOG = Logger.getLogger(AppStartupService.class.getName());
 
+	@Inject
+	private MemberRepository memberRepo;
+	
 	@PostConstruct
 	public void init() {
-		LOG.info("************** Hello World !!! *************. Ready to rock and roll");
+		Member m1 = new Member();
+		m1.setName("Test1");
+		m1.setAge(20);
+		memberRepo.addMember(m1);
+		
+		Member m2 = new Member();
+		m2.setName("Test2");
+		m2.setAge(20);
+		memberRepo.addMember(m2);
+		
+		memberRepo.findAll().forEach(System.out::println);
 	}
 	
 }
