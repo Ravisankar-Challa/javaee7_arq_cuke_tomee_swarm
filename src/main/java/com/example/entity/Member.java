e3c45dfa3775ff5089e1a735f3b4e62ba162e7d0
Ravisankar Challa <ravisankar.c@tcs.com>
2016-02-10 16:38:22 +1100

    Added Bean Validation to entity

--- src/main/java/com/example/entity/Member.java
+++ src/main/java/com/example/entity/Member.java
@@ -1,17 +1,31 @@
 package com.example.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Entity
+@Table(name="MEMBER_TABLE")
 public class Member {
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
+	
+	@NotNull
+	@Size(max=256)
+	@Column(name="MEMBER_NAME", length=32, nullable=false)
 	private String name;
+	
+	@NotNull
+	@Pattern(regexp="[0-9]*")
+	@Column(name="MEMBER_AGE")
 	private int age;
 
 	public Long getId() {
