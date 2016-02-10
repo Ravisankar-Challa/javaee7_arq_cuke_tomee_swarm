052a61c7c2d6dd84b2ca078abee7d48c24a1a4eb
Ravisankar Challa <ravisankar.c@tcs.com>
2016-02-10 16:02:29 +1100

    Added CDI

--- src/main/java/com/example/rest/HelloWorldResource.java
+++ src/main/java/com/example/rest/HelloWorldResource.java
@@ -1,18 +1,25 @@
 package com.example.rest;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import com.example.service.HelloService;
 
 @Path("/")
 public class HelloWorldResource {
 
+	@Inject
+	private HelloService helloService;
+	
 	@GET
-	@Path("sayhello")
+	@Path("sayhello/{name}")
 	@Produces(MediaType.TEXT_PLAIN)
-	public String sayHello() {
-		return "Hello World!!!";
+	public String sayHello(@PathParam("name") String name) {
+		return helloService.greetPerson(name);
 	}
 	
 }
